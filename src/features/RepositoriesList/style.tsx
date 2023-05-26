73d16982414bbf9f21165b05b52da20196971aa2
visneviySecret <visneviy.secret@yandex.ru>
2023-05-26 14:24:26 +0300

    fix uniq styles

--- src/features/RepositoriesList/style.tsx
+++ src/features/RepositoriesList/style.tsx
@@ -1,5 +1,23 @@
 import { styled } from 'styled-components'
 
+const colors = [
+    '#f6c6c6',
+    '#caab8e',
+    '#c1ca8e',
+    '#d39090',
+    '#a88564',
+    '#9faa5e',
+    '#8f7171',
+    '#776a5e',
+    '#56888a',
+    '#68b378',
+]
+
+const generateColor = (page: number) => {
+    console.log(colors[page])
+    return colors[page]
+}
+
 export const Title = styled.h3`
     margin-top: 1.5rem;
     font-size: 2rem;
@@ -10,11 +28,7 @@ export const ListWrapper = styled.div`
 `
 
 export const List = styled.ul<{ page: number }>`
-    background-color: #f6c6c6;
-    background-color: #${({ page }) =>
-            parseInt((page * 12).toString(), 16)
-                .toString()
-                .repeat(3)};
+    background-color: ${({ page }) => generateColor(page)};
     border-radius: 2rem;
     padding: 1rem;
     list-style: none;
