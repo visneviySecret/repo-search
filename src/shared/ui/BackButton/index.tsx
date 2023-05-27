c1efd9a582c2be84423d01dca5b860cc3ac44441
visneviySecret <visneviy.secret@yandex.ru>
2023-05-27 22:32:50 +0300

    updated back button functions

--- src/shared/ui/BackButton/index.tsx
+++ src/shared/ui/BackButton/index.tsx
@@ -1,10 +1,18 @@
 import { baseUrl } from '@/app/providers/router'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function BackButton() {
+    const query = useLocation().pathname
+
+    const getLink = () => {
+        const index = query.lastIndexOf('/')
+        const addedUrl = query.slice(0, index)
+        return `${baseUrl}${addedUrl}/`
+    }
+
     return (
         <button style={{ marginBottom: '50px' }}>
-            <Link to={`${baseUrl}/repo-list/`}>Go back!</Link>
+            <Link to={getLink()}>Go back!</Link>
         </button>
     )
 }
